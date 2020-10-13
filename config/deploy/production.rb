b7ee96a188713b9859a2d63906dd4d76adf46fd1
Eddie Stack <eddiestack90@gmail.com>
2020-10-13 17:28:24 +0100

    Changed Deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '207.154.249.154', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
